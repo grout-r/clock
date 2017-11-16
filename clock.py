36fe68d79d774f69b0fb6a8ad317bd3ddd3830ce
grout-r <roman.grout@epitech.eu>
2017-11-16 16:32:14 +0100

    push

--- clock.py
+++ clock.py
@@ -35,6 +35,7 @@ if __name__ == '__main__':
     PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
     PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
     # Create PCF8574 GPIO adapter.
+    print("ici")
     try:
         mcp = PCF8574_GPIO(PCF8574_address)
     except:
@@ -43,9 +44,9 @@ if __name__ == '__main__':
         except:
             print('I2C Address Error !')
         exit(1)
+    print("la")
     # Create LCD, passing in MCP GPIO adapter.
     lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4, 5, 6, 7], GPIO=mcp)
-
 
     t = Thread(target=run_scheduled)
     t.start()
