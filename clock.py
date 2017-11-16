c2270859fe3a4e92a75f37ab4cabde35886bf9e3
grout-r <roman.grout@epitech.eu>
2017-11-16 16:35:07 +0100

    push

--- clock.py
+++ clock.py
@@ -49,6 +49,9 @@ if __name__ == '__main__':
     print("la")
     # Create LCD, passing in MCP GPIO adapter.
     lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4, 5, 6, 7], GPIO=mcp)
+    mcp.output(3, 1)  # turn on LCD backlight
+    lcd.begin(16, 2)  #
+    lcd.message('ta mère la ktin ' + '\n')  #
 
     t = Thread(target=run_scheduled)
     t.start()
