e15ec641bd27b646c325760afeca4dc60afec0f3
grout-r <roman.grout@epitech.eu>
2017-11-16 15:23:46 +0100

    push

--- clock.py
+++ clock.py
@@ -30,6 +30,24 @@ def run_scheduled():
         time.sleep(1)
 
 if __name__ == '__main__':
+
+    PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
+    PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
+    # Create PCF8574 GPIO adapter.
+    try:
+        mcp = PCF8574(PCF8574_address)
+    except:
+        pass
+
+    try:
+            mcp = PCF8574(PCF8574A_address)
+    except:
+        print('I2C Address Error !')
+        exit(1)
+
+    # Create LCD, passing in MCP GPIO adapter.
+    lcd = LCD(pin_rs=0, pin_e=2, pins_db=[4, 5, 6, 7], GPIO=mcp)
+
     t = Thread(target=run_scheduled)
     t.start()
     app.run(host='0.0.0.0')
