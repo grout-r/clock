717e753f150c8e39aa445d5c0dfa42da254e6e1d
grout-r <roman.grout@epitech.eu>
2017-11-17 12:13:49 +0100

    push

--- lcd.py
+++ lcd.py
@@ -30,4 +30,4 @@ class lcd:
 
     def update(self, tmp):
         self.lcd.setCursor(0, 0)
-        self.lcd.message(datetime.now().strftime('%H:%M:%S\nIN:' + tmp.getTmp + "°C"))
+        self.lcd.message(datetime.now().strftime('%H:%M:%S\nIN:' + tmp.getTmp() + "°C"))
