1656623036e31e89ae7fc589c99dc27707b21830
grout-r <roman.grout@epitech.eu>
2017-11-17 17:25:46 +0100

    push

--- music.py
+++ music.py
@@ -1,19 +1,21 @@
 from gmusicapi import Mobileclient
 import vlc
+import json
 
 class music:
     def __init__(self):
         api = Mobileclient()
         api.login('roman.grout@gmail.com', 'romgroGMAIL95', Mobileclient.FROM_MAC_ADDRESS)
 
-        library = api.get_all_songs()
+
+       # library = api.get_all_songs()
 
 
-
+        station = api.get_all_stations()
+        print(json.dumps(station, sort_keys=True, indent=4, separators=(',', ': ')))
 
         url = api.get_stream_url(song_id='bc0c11b1-03ea-30a5-bda8-66906c532373', device_id=None, quality=u'hi')
         print(url)
-        print(api.get_all_stations())
 
         p = vlc.MediaPlayer(url)
         p.play()
