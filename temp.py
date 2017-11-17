5ddb8d14ed1c9c034c312013776778ca5e8cecaa
grout-r <roman.grout@epitech.eu>
2017-11-17 12:33:40 +0100

    push

--- temp.py
+++ temp.py
@@ -5,16 +5,15 @@ DHTPin = 11  # define the pin of DHT1
 class temp:
     def __init__(self):
         self.dht = DHT.DHT(DHTPin)
+        self.cache = "--C"
 
     def getTmp(self):
         chk = self.dht.readDHT11(DHTPin)
 
-        if (chk is self.dht.DHTLIB_OK):
-            print("DHT11,OK!")
-        elif (chk is self.dht.DHTLIB_ERROR_CHECKSUM):  # data check has errors
-            print("DHTLIB_ERROR_CHECKSUM!!")
+        if (chk is self.dht.DHTLIB_OK or chk is self.dht.DHTLIB_ERROR_CHECKSUM):
+            self.cache = '{:02d}'.format(self.dht.temperature)
         elif (chk is self.dht.DHTLIB_ERROR_TIMEOUT):  # reading DHT times out
             print("DHTLIB_ERROR_TIMEOUT!")
         else:  # other errors
             print("Other error!")
-        return '{:02d}'.format(self.dht.temperature)
+        return self.cache
