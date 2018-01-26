29c64c3538a65b94033486867beede9d1dc5f063
Cedric Voinnet <cedricvoinnet@gmail.com>
2018-01-26 16:36:05 +0100

    Check d'args pour add

--- clock.py
+++ clock.py
@@ -17,15 +17,25 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route("/add")
+@app.route('/add')
 def add():
-    schedule.every().day.at(request.args.get("time")).do(ring)
-    return "coucou"
+    if 'time' not in request.args or not check_time(request.args['time']):
+        return 'grosse erreur mamene', 400
+    schedule.every().day.at(request.args.get('time')).do(ring)
+    return "coucou", 200
 
+def check_time(time):
+    try:
+        time = time.split(':')
+        if len(time) != 2 or int(time[0]) not in range(0, 24) or int(time[1]) not in range(0, 60):
+            return False
+    except ValueError as e:
+        return False
+    return True
 
 def ring():
     print("ring")
-    lamp.switch_on()
+    # lamp.switch_on()
     pass
 
 
@@ -43,9 +53,9 @@ if __name__ == '__main__':
     tmp = temp()
     lamp = Lamp()
 
-    # music = Music()
-    # music.load_random()
-    # music.play_random()
+    music = Music()
+    music.load_random()
+    music.play_random()
 
     buttonPin = 12  # define the buttonPin
     GPIO.setmode(GPIO.BOARD)
@@ -54,4 +64,3 @@ if __name__ == '__main__':
     t = Thread(target=loop)
     t.start()
     app.run(host='0.0.0.0')
-
